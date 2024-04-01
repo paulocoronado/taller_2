13b825bfd093bc1e5f93d9f9db95258509fb6fde
Paulo César Coronado <paulocoronado@udistrital.edu.co>
2024-04-01 15:38:15 -0500

    intro

--- introPython2.py
+++ introPython2.py
@@ -1,33 +1,15 @@
-# while
-# while <condición_verdadera>:
-#    cuerpo del ciclo
-# Condiciones son: expresiones booleanas (or, and) y operaciones de comparación
-# Ciclos controlados por un evento
+# Si no tengo un iterable puedo usar la función
+# range() para generar una secuencia de números
 
-a=0
-while 1==1:
-    
-    a= int(input("Ingrese un número"))
-    
-    if a==10:
-        break
-        
-    
-    
+for x in range(-10, 10):
+    print(x)
+
+# Taller crear un programa que pida un número al usuario y:
+# 1. Imprima los números impares entre -número y número
+# 2. Imprima los número primos entre 0 y número*100
+# El programa debe garantizar que el usuario ingrese un número >0
+
+#Entrega antes de las 4 en el repositorio remoto
 
 
 
-    
-
-    
-        
-    
-    
-    
-
-
-
-
-
-
-# for
